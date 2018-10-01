8beb09166649971005fbe33f401d26f9bd893468
Administrador <eduhernandezxd@gmail.com>
2018-10-01 17:14:57 -0400

    Version 8.0 Probando agregar la busqueda de los mapa
    Signed-off-by: Administrador <eduhernandezxd@gmail.com>

--- app/src/main/java/com/ucab/tesis/imac/Main2Activity.java
+++ app/src/main/java/com/ucab/tesis/imac/Main2Activity.java
@@ -1,9 +1,9 @@
 package com.ucab.tesis.imac;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.ucab.tesis.imac.fragments.FragmentA;
 import com.ucab.tesis.imac.fragments.FragmentB;
 import com.ucab.tesis.imac.fragments.FragmentFOTO;
@@ -32,7 +32,6 @@ public class Main2Activity extends AppCompatActivity
     FragmentA fragmentA;
     FragmentB fragmentB;
     FragmentFOTO fragmentFOTO;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +52,9 @@ public class Main2Activity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(),MapaActivity.class);
+                startActivity(intent);
+
             }
         });
 
